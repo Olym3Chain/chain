bbd3a127b97f9304119882d9522a073ffae5066f
Oym3 Protocol <olym3protocol@gmail.com>
2025-07-12 13:07:36 +0700

    Update network_name.go

--- Olym3-Testnet-S3/network_name.go
+++ Olym3-Testnet-S3/network_name.go
@@ -1,9 +1,58 @@
+                                                                                                                                                                                            /home/erigon/erigon/erigon-lib/chain/networkname/network_name.go                                                                                                                                                                                                       
+// Copyright 2024 The Erigon Authors
+// This file is part of Erigon.
+//
+// Erigon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Erigon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Erigon. If not, see <http://www.gnu.org/licenses/>.
+
+package networkname
+
 const (
-    ...
-    Olym3TestnetS3 = "olym3-testnet-s3"
+        Mainnet             = "mainnet"
+        Holesky             = "holesky"
+        Sepolia             = "sepolia"
+        Hoodi               = "hoodi"
+        Dev                 = "dev"
+        Amoy                = "amoy"
+        BorMainnet          = "bor-mainnet"
+        BorDevnet           = "bor-devnet"
+        Gnosis              = "gnosis"
+        BorE2ETestChain2Val = "bor-e2e-test-2Val"
+        Chiado              = "chiado"
+        Test                = "test"
+        Olym3TestnetS3      = "olym3-testnet-s3"
+
 )
 
+
 var All = []string{
-    ...
-    Olym3TestnetS3,
+        Mainnet,
+        Holesky,
+        Sepolia,
+        Hoodi,
+        Amoy,
+        BorMainnet,
+        BorDevnet,
+        Gnosis,
+        Chiado,
+        Test,
+        Olym3TestnetS3,
 }
+
+
+
+
+
+
+
+
